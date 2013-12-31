1df51273ecbf115d248b4c811ed3e671056fd144
Jonathan Ruiz <jonathan.ruiz@sportpursuit.co.uk>
2013-12-31 10:11:12 +0100

    Basic plugin layout beta

---
+++ app/code/community/Hubun/XmppSupport/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jruiz
+ * Date: 12/30/13
+ * Time: 7:06 PM
+ */ 
+class Hubun_XmppSupport_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/Hubun/XmppSupport/sql/hubun_xmppsupport_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jruiz
+ * Date: 12/30/13
+ * Time: 7:06 PM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
